63d770fbb044bb8a4a9ced3ac25e3517844a4928
Rowand Mohammad <rowandnajim@gmail.com>
2021-10-09 00:05:11 +0100

    Initialising assembly folder files in unity to link  tests to scripts for TDD.

---
+++ Survivor/Assets/Tests/EditMode/MouseViewTests.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+namespace Tests{
+
+
+    public class MouseViewTests 
+    {
+        [Test]
+        public void MouseViewer_Test(){
+            var mouseView = new MouseView();
+
+        }
+
+    }
+}
