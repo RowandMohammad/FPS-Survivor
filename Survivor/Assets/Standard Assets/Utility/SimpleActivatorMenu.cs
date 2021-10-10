2e1f2f26bad751921db04ba1018577c3c0103391
Rowand Mohammad <rowandnajim@gmail.com>
2021-10-10 19:30:50 +0100

    Finished initialising unit case setups and modified some class names.

--- Survivor/Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
+++ Survivor/Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
@@ -1,3 +1,40 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4f2dd7e302128e85c5d00f590c63731e95b6968e94af731ebe136d04148a7414
-size 1093
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+#pragma warning disable 618
+namespace UnityStandardAssets.Utility
+{
+    public class SimpleActivatorMenu : MonoBehaviour
+    {
+        // An incredibly simple menu which, when given references
+        // to gameobjects in the scene
+        public Text camSwitchButton;
+        public GameObject[] objects;
+
+
+        private int m_CurrentActiveObject;
+
+
+        private void OnEnable()
+        {
+            // active object starts from first in array
+            m_CurrentActiveObject = 0;
+            camSwitchButton.text = objects[m_CurrentActiveObject].name;
+        }
+
+
+        public void NextCamera()
+        {
+            int nextactiveobject = m_CurrentActiveObject + 1 >= objects.Length ? 0 : m_CurrentActiveObject + 1;
+
+            for (int i = 0; i < objects.Length; i++)
+            {
+                objects[i].SetActive(i == nextactiveobject);
+            }
+
+            m_CurrentActiveObject = nextactiveobject;
+            camSwitchButton.text = objects[m_CurrentActiveObject].name;
+        }
+    }
+}
