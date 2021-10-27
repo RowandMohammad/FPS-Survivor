6f8b722bcdf7d47b71acf642103b1484b565dec9
Rowand Mohammad <rowandnajim@gmail.com>
2021-10-27 09:02:35 +0100

    Added new running jump animation feature

--- Survivor/Assets/Scripts/PlayerMovement/MovementAnimator.cs
+++ Survivor/Assets/Scripts/PlayerMovement/MovementAnimator.cs
@@ -8,6 +8,9 @@ public class MovementAnimator : MonoBehaviour
     private Movement Movement;
     private SprintAndCrouch sprintAndCrouch;
     public Animator _animator;
+    public bool isSprintJump;
+    public bool isSprinting;
+    public bool isJumping;
 
     [Header("Keybinds")]
     [SerializeField] KeyCode sprintKey = KeyCode.LeftShift;
@@ -32,6 +35,7 @@ public class MovementAnimator : MonoBehaviour
         animateJump();
         animateSprinting();
         animateCrouch();
+        animateSprintJump();
     }
 
     void animateMovementLocomotion()
@@ -45,13 +49,18 @@ public class MovementAnimator : MonoBehaviour
 
     void animateJump()
     {
-        if (Input.GetKeyDown(jumpKey) && playerMovement.playerIsGrounded)
+        if (Input.GetKeyDown(jumpKey) && playerMovement.playerIsGrounded && !isSprinting)
         {
+            
             _animator.SetBool("isJumping", true);
+            isJumping = true;
+
         }
-        if (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("isJumping") && _animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
         {
+            
             _animator.SetBool("isJumping", false);
+            isJumping = false;
 
         }
 
@@ -61,12 +70,16 @@ public class MovementAnimator : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.LeftShift) && !sprintAndCrouch.isCrouching)
         {
+            
             _animator.SetBool("isSprinting", true);
+            isSprinting = true;
         }
 
         if (Input.GetKeyUp(KeyCode.LeftShift) && !sprintAndCrouch.isCrouching)
         {
+            
             _animator.SetBool("isSprinting", false);
+            isSprinting = false;
         }
     }
 
@@ -79,6 +92,19 @@ public class MovementAnimator : MonoBehaviour
         if (!sprintAndCrouch.isCrouching)
         {
             _animator.SetBool("isCrouching", false);
+        }
+    }
+
+    void animateSprintJump()
+    {
+        if (isSprinting && Input.GetKeyDown(jumpKey) && playerMovement.playerIsGrounded)
+        {
+            _animator.SetBool("isSprintJumping", true);
+        }
+
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Jumping Sprint") && _animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+        {
+            _animator.SetBool("isSprintJumping", false);
         }
     }
 }
