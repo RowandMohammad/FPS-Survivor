d4f6c283fcd71ad62efa89651eca92275c836b33
Rowand Mohammad <rowandnajim@gmail.com>
2021-10-31 14:53:02 +0000

    Setup up Survivor project with photon 2 application online

---
+++ Survivor/Assets/Tests/EditMode/LauncherTests.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class LauncherTests
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void LauncherTestsSimplePasses()
+    {
+        Launcher test = new Launcher();
+    }
+}
