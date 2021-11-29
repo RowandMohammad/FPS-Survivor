8629273a174412372e4c98b7f72d31004fa7d050
Rowand Mohammad <rowandnajim@gmail.com>
2021-11-29 18:26:37 +0000

    feat (Zombie ai have animations when taking damage)

--- Survivor/Assets/Scripts/AI/BasicZombieController.cs
+++ Survivor/Assets/Scripts/AI/BasicZombieController.cs
@@ -1,11 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BasicZombieController : MonoBehaviour, IDamageable
 
 {
     public float health = 100f;
+    [SerializeField] public Animator _animator;
+    System.Random _random = new System.Random();
+
+    private void awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +30,23 @@ public class BasicZombieController : MonoBehaviour, IDamageable
 
     public void TakeDamage(float damage)
     {
+        
         health -= damage;
         print(health);
+        _animator.SetInteger("takeDamageIndex", Random.Range(0, 2));
+        _animator.SetTrigger("takeDamage");
+
+
+
+
+
+
     }
+
+    int animationRandomizer()
+    {
+        return _random.Next(1, 2);
+        
+    }
+
 }
