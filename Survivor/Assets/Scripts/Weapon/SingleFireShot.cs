b890890ccb542df02e69b80946697cc180b590aa
Rowand Mohammad <rowandnajim@gmail.com>
2021-11-30 00:50:25 +0000

    feat ( Allow for tracer effect upon shooting)

--- Survivor/Assets/Scripts/Weapon/SingleFireShot.cs
+++ Survivor/Assets/Scripts/Weapon/SingleFireShot.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SingleFireShot : Weapon
 {
 	[SerializeField] Camera cam;
+	public ParticleSystem hitEffect;
 
 
 	void Awake()
@@ -22,9 +23,13 @@ public class SingleFireShot : Weapon
 		RaycastHit hit;
 		Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 		ray.origin = cam.transform.position;
+		
 		if (Physics.Raycast(ray, out hit))
 		{
 			hit.collider.gameObject.GetComponent<IDamageable>()?.TakeDamage(((WeaponInfo)itemObjectInfo).damage);
+			hitEffect.transform.position = hit.point;
+			hitEffect.transform.forward = hit.normal;
+			hitEffect.Emit(1);
 		}
 	}
 }
