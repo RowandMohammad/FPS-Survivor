8242f6061a2c7490cad4bc40c39fd5f5cdaccf5c
Rowand Mohammad <rowandnajim@gmail.com>
2022-03-20 14:31:30 +0000

    UI ( Button Animation Updates)

---
+++ Survivor/Assets/UI/Buttons/MenuButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+	[SerializeField] MenuButtonController menuButtonController;
+	[SerializeField] Animator animator;
+	[SerializeField] AnimatorFunctions animatorFunctions;
+	[SerializeField] int thisIndex;
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(menuButtonController.index == thisIndex)
+		{
+			animator.SetBool ("Selected", true);
+			if(Input.GetAxis ("Submit") == 1){
+				animator.SetBool ("Pressed", true);
+			}else if (animator.GetBool ("Pressed")){
+				animator.SetBool ("Pressed", false);
+				animatorFunctions.disableOnce = true;
+			}
+		}else{
+			animator.SetBool ("Selected", false);
+		}
+    }
+}
